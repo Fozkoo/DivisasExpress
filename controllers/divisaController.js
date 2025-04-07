025c1276d4ba4337067e4e5d4da9ac9c12b78ec4
Tiziano Martinelli <tmartinelli@alumnos.unsada.edu.ar>
2025-04-07 17:24:44 -0300

    feat: create getById endpoint

--- controllers/divisaController.js
+++ controllers/divisaController.js
@@ -8,11 +8,19 @@ export class DivisaController{
     }
 
 
-    // Este es el endpoint que queda para hacer 
     getDivisaById = async (req, res) => {
-
+        const { id } = req.params;
+        try {
+            const divisa = await this.DivisaModel.findOne({ where: { id } });
+            if (!divisa) {
+                return res.status(404).json({ error: 'Divisa no encontrada' });
+            }
+            res.json(divisa);
+        } catch (error) {
+            console.error('Error al obtener la divisa:', error);
+            res.status(500).json({ error: 'Error al obtener la divisa' });
+        }
     }
-
 
     getAllDivisas = async (req, res) => {
         try{
--- routes/divisaRoute.js
+++ routes/divisaRoute.js
@@ -7,7 +7,7 @@ export const createDivisaRouter = ({ divisaModel }) => {
     const divisaController = new DivisaController(divisaModel);
 
     divisaRouter.get('/', (req, res) => divisaController.getAllDivisas(req,res));
-
+    divisaRouter.get('/:id', (req, res) => divisaController.getDivisaById(req,res));
     
     return divisaRouter;
 }
